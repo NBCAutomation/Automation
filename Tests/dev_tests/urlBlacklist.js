546f96f78b6df12f5216b712ea39aeceef64d05d
Deltrie Allen <deltrie.allen@nbcuni.com>
2015-11-18 16:47:19 -0500

    Updating blacklist check

--- Tests/dev_tests/urlBlacklist.js
+++ Tests/dev_tests/urlBlacklist.js
@@ -7,25 +7,54 @@
 * casperjs test 404Spider.js --includes=urlBlacklist.js --url=http://www.nbcmiami.com/
 */
 
+var helpers = require('helper')
+
 casper.options.onResourceRequested = function(casper, requestData, request) {
     // If any of these strings are found in the requested resource's URL, skip
     // this request. These are not required for running tests.
+    
+    var parser = document.createElement('a');
+    parser.href = casper.cli.get('url');
+
+    newUrl = parser.href;
+    var sourceString = newUrl.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
+
     var skip = [
+        //Google exclusions
+        'google.com',
         'googleads.g.doubleclick.net',
         'cm.g.doubleclick.net',
         'www.googleadservices.com',
+        'googleapis.com',
+        'partner.googleadservices.com',
+
+        'data:image/png',
+        'intellicast.com',
+        'chartbeat.com',
         'static.chartbeat.com',
         'ping.chartbeat.com',
-        'fonts.googleapis.com',
-        'partner.googleadservices.com',
+        'quantserve.com',
+        'sigalalert.com',
+        'krxd.com',
+        
+        //Othe rexclusions
+        'nbc.com',
+        'nbc.com/app_link/nbctve',
+        'nbcudigitaladops.com',
+        'tidaltv.com',
+        'tremormedia.com',
         'services.intellicast.com',
-        'www.facebook.com',
-        'platform.twitter.com'
+        'facebook.com',
+        'platform.twitter.com',
+        'taboola.com',
+        'betrad.com',
+        'adreadytractions.com',
+        'everesttech.net',
+        'scorecardresearch.com'
     ];
 
-    skip.forEach(function(needle) {
-        if (requestData.url.indexOf(needle) > 0) {
-            request.abort();
-        }
-    })
+    if (requestData.url.indexOf( sourceString ) == -1 ) {
+        request.abort();
+        // console.log( 'skipping --  ' + requestData.url )
+    }
 };
