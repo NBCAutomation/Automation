4e564b5cf23f93f7b84f70dfd6b92e01257847ca
Deltrie Allen <deltrie.allen@gmail.com>
2016-04-14 17:21:53 -0400

    Updating the base class, staging functions

--- libraries/Base/spire_base.php
+++ libraries/Base/spire_base.php
@@ -1,5 +1,7 @@
 <?php
-//Database/Model
+use \InvalidArgumentException;
+use Psr\Http\Message\ResponseInterface;
+
 class Spire {
 
 	public function getGravatar( $email, $s = 250, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
@@ -25,4 +27,83 @@ class Spire {
 	    return $dbh;
 	}
 	
+	public function dirFilesToArray($dir) {
+
+		$result = array(); 
+
+		$cdir = scandir($dir);
+
+		foreach ($cdir as $key => $value) {
+			if ( !in_array($value,array(".","..")) ) {
+
+				if ( is_dir($dir . DIRECTORY_SEPARATOR . $value) ) {
+					$result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+				} else {
+					$result[] = $value;
+				}
+			}
+		}
+		return $result;
+	}
+
+	public function readCSV($__file) {
+		$row = 1;
+		$fails = 0;
+
+		$__fileData;
+
+		$__fileData .= "<table class=\"table table-bordered table-striped\">";
+
+		if (($handle = fopen($__file, "r")) !== FALSE) {
+		    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+		        $num = count($data);
+		        $row++;
+
+		        $__fileData .= "<tr>";
+
+		        for ($c = 0; $c < $num; $c++) {
+		        	
+		        	if (strpos($data[$c], 'Fail') !== false || strpos($data[$c], 'FAIL') !== false) {
+		        		$fails++;
+		        		$__class = 'class="test_fail" ';
+		        	} else {
+		        		$__class = '';
+		        	}
+
+		        	if (!empty($data[$c])) {
+		        		if ($row < 5) {
+		        			$__fileData .= "<td colspan=\"5\">" . $data[$c] . "</td>";
+		        		} else {
+		        			if ($row < 7) {
+		        				$__fileData .= "<td>" . $data[$c] . "</td>";
+			        		} else {
+			        			$__fileData .= "<td ". $__class .">" . $data[$c] . "</td>";
+			        		}
+		        		}
+		        	}
+		        }
+		        $__fileData .= "</tr>";
+		    }
+		    fclose($handle);
+		}
+		$__fileData .= "</table>";
+		return $__fileData;
+	}
+
+	public function dirToArray($dir) {
+
+		$result = array(); 
+
+		$cdir = scandir($dir);
+
+		foreach ($cdir as $key => $value) {
+			if ( !in_array($value,array(".","..")) ) {
+
+				if ( is_dir($dir . DIRECTORY_SEPARATOR . $value) ) {
+					$result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+				}
+			}
+		}
+		return $result;
+	}
 }
