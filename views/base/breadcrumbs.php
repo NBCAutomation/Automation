f027c040f78e74d2d8f6641e620a810da994b936
Deltrie Allen <deltrie.allen@gmail.com>
2016-04-15 19:15:41 -0400

    Updated breadcrumbs

--- views/base/breadcrumbs.php
+++ views/base/breadcrumbs.php
@@ -1,18 +1,27 @@
 <?php
 	$urlPath = explode( '/', $viewPath );
+	$__endItem = end($urlPath);
+
 	$breadcrumbPath;
+	if (sizeof($urlPath) > 1) {
+		$baseView = $urlPath[0];
+	}
+
 	foreach ($urlPath as $key => $value) {
+			// echo $key." >> ".$value;
 		if ($key > 0) {
-			if ($value == end($urlPath)) {
+			if ($value == $__endItem) {
 				$breadcrumbPath .= '<li>'. $value .'</li>';
 			} else {
-				$breadcrumbPath .= '<li><a href=/reports/'. $urlPath[0] .'/'. $value .'/main>'. $value .'</a></li>';
+				$urlPath = $page_name.'/'.$urlPath[0].'/'.$value.'/main';
+				$breadcrumbPath .= '<li><a href=/'.$urlPath.'>'. $value .'</a></li>';
 			}
 		} else {
-			$breadcrumbPath .= '<li><a href=/reports/'. $urlPath[$key] .'>'. $value .'</a></li>';
+			$breadcrumbPath .= '<li><a href=/'.$page_name.'/'.$value.'>'. $value .'</a></li>';
 		}
 	}
 ?>
+
 <div id="breadcrumbs">
 	<ul>
 		<li><a href="/"><i class="fa fa-home" aria-hidden="true"></i></a></li>
