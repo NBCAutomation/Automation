2ccc68c83d1dd509459773956bba55e06f65a337
Deltrie Allen <deltrie.allen@gmail.com>
2016-09-29 14:44:59 -0400

    timezone

--- templates/reports.php
+++ templates/reports.php
@@ -1,4 +1,8 @@
-<?php include_once 'base/header.php'; ?>
+<?php
+	include_once 'base/header.php';
+
+	date_default_timezone_set('UTC');
+?>
 	<div class="panel-body api_results">
 	<?php if ($mainView) {
 		?>
