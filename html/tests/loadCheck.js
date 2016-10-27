d2b501e3a4c195da8a77487101568e8a50379877
Deltrie Allen <deltrie.allen@gmail.com>
2016-10-27 16:27:04 -0400

    Staging new tests load check and omniture tests

--- html/tests/loadCheck.js
+++ html/tests/loadCheck.js
@@ -11,35 +11,92 @@ var saveLoc = ('screenshots/');
 
 casper.test.begin('Page laod/wrapper tests', function suite(test) {
 
-    casper.start( siteUrl, function(response) {
+    // Output variables
+    var type = casper.cli.get('output');
         
-        // require('utils').dump(response);
+    if (type === 'debug') {
+        var debugOutput = true;
+    } else if (type === 'dictionary') {
+        var createDictionary = true;
+        var logResults = false;
+    } else if (type === 'console') {
+        var showOutput = true;
+    }
+
+
+    if ( casper.cli.get('testing') ) {
+        var logResults = false;
+    }
+
+    // Start testing
+    casper.start( siteUrl, function(response) {
 
         if ( response.status == 200 ) {
             no_error = true;
         } else {
-            this.echo('Page not loaded correctly. Response: ' + response.status).exit();
+            throw new Error('Page not loaded correctly. Response: ' + response.status);
+            this.exit();
         }
 
         casper.then(function() {
             if ( no_error ) {
-                test.assertSelectorHasText('body', 'nbc', "NBC Property");
 
-                test.assertSelectorHasText('body', 'home', "Homepage loaded");
+                casper.open(siteUrl,{ method: 'get', headers: { 'customerID': '8500529', 'useremail': 'discussion_api@clickability.com' } }).then(function(resp) {
 
-                test.assertExists('#masthead', "The masthead area loaded correctly.");
-                    test.assertVisible('#logocont img', "...is visible.");
+                
+                var propertyType = document.getElementsByTagName("body");
+                console.log(propertyType);
+                console.log(propertyType.classList.item("nbc"));
+                
+                for(var i = 0; i < propertyType.length; i++) {
+                    console.log( propertyType[i].nodeName );
+                    
+                }
 
-                test.assertExists('#body', "The body area loaded correctly.");
-                    test.assertVisible('#body', "...is visible.");
+                if ( propertyType.className.indexOf === 'nbc' ) {
+                    console.log('ots property...');
+                } else {
+                    console.log('tlm property...');
+                }
 
-                test.assertExists('#nav', "The nav loaded correctly.");
-                    test.assertVisible('#nav', "...is visible.");
+                // console.log('Page title: >> ' + this.getTitle());
+                ;
+        // this.test.assertNotEquals('body', 'nbc', 'PASS');
 
-                test.assertExists('#footer', "The footer area loaded correctly.");
-                    test.assertVisible('#footer', "...is visible.");
+                // test.assertSelectorHasText('body', 'home', "Homepage loaded");
+
+                // test.assertExists('.site-header', "The site header loaded correctly.");
+                // test.assertVisible('.site-header', "...is also visible.");
+                // test.assertExists('.brand a img', "The logo loaded correctly.");
+                // test.assertVisible('.brand a', "...is also visible.");
+                // console.log('[ -- clicking logo -- ]');
+                // this.click('.brand a');
+
+                });
             }
         });
+
+        // Hader tests
+        casper.then(function() {
+            // console.log('clicked ok, new location is ' + this.getCurrentUrl());
+
+            // test.assertExists('#body', "The body area loaded correctly.");
+            //     test.assertVisible('#body', "...is visible.");
+
+            // test.assertExists('#nav', "The nav loaded correctly.");
+            //     test.assertVisible('#nav', "...is visible.");
+
+            // test.assertExists('#footer', "The footer area loaded correctly.");
+            //     test.assertVisible('#footer', "...is visible.");
+        });
+
+        // // Page body tests
+        // casper.then(function() {
+        //     test.assertExists('#spotlight', "The spotlight area loaded correctly.");
+        //     test.assertExists('#top-stories', "The top stories area loaded correctly.");
+        //     test.assertExists('#primary', "The top stories area loaded correctly.");
+        //     test.assertExists('#footer', "The footer area loaded correctly.");
+        // });
 
     }).run(function() {
         test.done();
