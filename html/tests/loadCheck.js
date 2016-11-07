63d83e7b0da000c192544163530a58b742dce8d8
Deltrie Allen <deltrie.allen@gmail.com>
2016-11-07 09:46:25 -0500

    blergh

--- html/tests/loadCheck.js
+++ html/tests/loadCheck.js
@@ -4,16 +4,57 @@
 // Case: PASS/FAIL Checks to see if a page is loaded as well as if the page elements are loaded and visible.
 // Use: casperjs [file_name] --url="[site_url]"
 
+// like interactive radar map - layers
+// the connect dropdown- and linkouts in that
+// thats all visual
+
 
 casper.test.begin('Page laod/wrapper tests', function suite(test) {
 
+    // Config vars
     var utils = require('utils');
     var siteUrl = casper.cli.get("url");
     var mouse = require("mouse").create(casper);
     var saveLoc = ('screenshots/');
     var otsTestSuite = false;
     var tlmTestSuite = false;
-    // viewportSize : { width: 1280, height: 5000 }
+    casper.options.viewportSize = { width: 1280, height: 5000 };
+
+
+    // Util vars
+    var currentTime = new Date();
+
+    var month = currentTime.getMonth() + 1;
+    var day = currentTime.getDate();
+    var year = currentTime.getFullYear();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+
+        if (minutes < 10){
+            minutes = "0" + minutes;
+        }
+
+        if(hours > 11){
+            var toD = "PM";
+        } else {
+            var toD = "AM";
+        }
+
+        if (hours === '0'){
+            var hours = "12";
+        }
+
+
+
+    var timeStamp = month+'_'+day+'_'+year+'-'+hours+'_'+minutes+'-'+toD;
+
+    var parser = document.createElement('a');
+    parser.href = casper.cli.get('url');
+
+    newUrl = parser.href;
+    var sourceString = newUrl.replace('http://','').replace('https://','').replace('www.','').replace('.com','').split(/[/?#]/)[0];
+    var urlUri = sourceString.replace('.','_');
+
 
     // Output variables
     var type = casper.cli.get('output');
@@ -66,58 +107,48 @@ casper.test.begin('Page laod/wrapper tests', function suite(test) {
                     var tlmTestSuite = true;
                 }
 
-                console.log('Page title: >> ' + this.getTitle());
-                test.assertSelectorHasText('body', 'home', "Homepage loaded");
+                test.comment('Page title: >> ' + this.getTitle());
 
                 // Set testing item
                 if (otsTestSuite) {
                     casper.waitUntilVisible('.sfbox', function(){
-                            this.test.assertNotEquals('body', 'nbc', 'OTS Body class set');
-                            
-                            test.assertExists('.site-header', "The site header loaded correctly.");
-                                test.assertVisible('.site-header', "...is also visible.");
-                            
-                            test.assertExists('.brand a img', "The logo loaded correctly.");
-                                test.assertVisible('.brand a', "...is also visible.");
+                        test.comment('Visual assertions/tests');
+                        test.comment('loading done.....');
+                        test.assertSelectorHasText('body', 'home', "Homepage loaded");
 
-                            test.assertExists('.navbar', "The nav loaded correctly.");
-                                test.assertVisible('.navbar', "...is visible.");
-
-                            test.assertExists('.nav-small-section.nav-live-tv', ".nav-small-section.nav-live-tv loaded correctly.");
-                                test.assertVisible('.nav-small-section.nav-live-tv', "...is visible.");
-
-                                
+                        this.test.assertNotEquals('body', 'nbc', 'OTS Body class set');
                         
-                            console.log('sfbox visible');
-                            casper.mouse.move('.nav-small-section.nav-live-tv');
-                            
-                        // casper.waitUntilVisible('.nav-small-section.nav-live-tv .nav-small-sub', function(){
-                            // this.click(expectedElementSelector);
-                            // test.assertVisible('.nav-small-section.nav-live-tv .nav-small-sub', "tv subnav...is visible.");
-                            // casper.capture('screenshots/mouse-hover-screenshot.png');
+                        test.assertExists('.site-header', "The site header loaded correctly.");
+                            test.assertVisible('.site-header', "...is also visible.");
                         
+                        test.assertExists('.brand a img', "The logo loaded correctly.");
+                            test.assertVisible('.brand a', "...is also visible.");
 
-                        this.on('mouse.move', function() {
-                            console.log('mouse moved');
-                            // phantomcss.screenshot('.selector', 'screenshotname-hover');
-                            casper.capture('screenshots/mouse-hover-screenshot.png');
-                        });
+                        test.assertExists('.breaking-bar', "The breaking-bar is enabled and loaded correctly.");
+                            test.assertVisible('.breaking-bar', "...is visible.");
 
+                        test.assertExists('.navbar', "The nav loaded correctly.");
+                            test.assertVisible('.navbar', "...is visible.");
+
+                        test.assertExists('.nav-small-section.nav-live-tv', ".nav-small-section.nav-live-tv loaded correctly.");
+                            test.assertVisible('.nav-small-section.nav-live-tv', "...is visible.");
+
+                        this.mouse.move('.nav-small-section.nav-live-tv a');
+                            test.assertVisible('.nav-small-section.nav-live-tv .nav-small-sub', "tv subnav...is visible.");
+                            this.captureSelector('screenshots/' + urlUri + '_mouse-hover-screenshot' + timeStamp + '.png', 'body');
+                            test.comment('tv subnav screenshot captured.');
 
                         test.assertExists('.weather-module', "The weather module loaded correctly.");
                             test.assertVisible('.weather-module', "...is visible.");
                         
                         test.assertExists('.weather-module-radar iframe', "The weather radar loaded correctly.");
                             test.assertVisible('.weather-module-radar iframe', "...is visible.");
-                        
+
+                        test.assertExists('.sfbox', "The spreadfast modules loaded correctly.");
+                            test.assertVisible('.sfbox', "...is visible.");
 
                         test.assertExists('.footer', "The footer area loaded correctly.");
                             test.assertVisible('.footer', "...is visible.");
-                        
-                        // casper.capture('screenshots/this-screenshot.png');
-
-                        console.log('[ -- clicking logo -- ]');
-                        this.click('.nav-small-section.nav-live-tv a');
                     });
 
                 } else {
@@ -136,9 +167,6 @@ casper.test.begin('Page laod/wrapper tests', function suite(test) {
 
                     test.assertExists('.page_footer', "The footer area loaded correctly.");
                         test.assertVisible('.page_footer', "...is visible.");
-
-                    console.log('[ -- clicking logo -- ]');
-                    this.click('#logocont a');
                 }
 
                 });
@@ -146,12 +174,16 @@ casper.test.begin('Page laod/wrapper tests', function suite(test) {
         });
 
         // Action tests
-        casper.then(function() {
-            // if (otsTestSuite) {
-                console.log('clicked ok, new location is ' + this.getCurrentUrl());
-            // } else {
- 
-            // } 
+        casper.then(function(otsTestSuite) {
+
+            if (otsTestSuite) {
+                test.comment('[ -- clicking logo -- ]');
+                    this.mouse.click('.brand a');
+                test.comment('clicked ok, new location is ' + this.getCurrentUrl());
+            } else {
+                test.comment('[ -- clicking logo -- ]');
+                this.click('#logocont a');
+            } 
         });
     }).run(function() {
         test.done();
