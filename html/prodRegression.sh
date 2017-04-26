2900dc7f5fed8be1c0dfda1a7bbbc0d005744bec
Steve B <steve.birstok@nbcuni.com>
2017-04-26 11:47:34 -0400

    CASPER_PATH instead of NODE_PATH

--- html/prodRegression.sh
+++ html/prodRegression.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-NODE_PATH="node_modules/.bin";
-cat sites.txt | xargs -P1 -I{} $NODE_PATH/casperjs test "./tests/regressionTest.js" --url="{}";
-cat sites.txt | xargs -P1 -I{} $NODE_PATH/casperjs test "./tests/regressionTest.js" --url="{}" --engine=slimerjs
+CASPER_PATH="node_modules/.bin";
+cat sites.txt | xargs -P1 -I{} $CASPER_PATH/casperjs test "./tests/regressionTest.js" --url="{}";
+cat sites.txt | xargs -P1 -I{} $CASPER_PATH/casperjs test "./tests/regressionTest.js" --url="{}" --engine=slimerjs
