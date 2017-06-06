d789beefb869b65b7119e5e635c187824403ed98
Deltrie Allen <deltrie.allen@gmail.com>
2017-06-06 09:07:09 -0400

    blaaaaaah

--- src/dbHandler.php
+++ src/dbHandler.php
@@ -728,7 +728,9 @@ class DbHandler {
             $db_con = Spire::getConnection();
 
             // $stmt = $db_con->prepare("SELECT AVG(loadtime) AS averageLoadTime, test_type AS dataPointName, Date(created) AS dayDate, Hour(created) AS hourInterval FROM loadtimes GROUP BY test_type, DAY(created), HOUR(created)");
-            $stmt = $db_con->prepare("SELECT AVG(loadtime) AS averageLoadTime, test_type AS loadTimeFrom, Date(created) AS date FROM loadtimes GROUP BY test_type, DAY(created)");
+            // $stmt = $db_con->prepare("SELECT AVG(loadtime) AS averageLoadTime, test_type AS loadTimeFrom, Date(created) AS date FROM loadtimes GROUP BY test_type, DAY(created)");
+
+            $stmt = $db_con->prepare("SELECT AVG(loadtime) AS averageLoadTime, test_type AS loadTimeFrom, Date(created) AS date FROM loadtimes WHERE Month(created) = Month(CURRENT_TIMESTAMP()) GROUP BY test_type, DAY(created)");
 
             $loadTimeArray = array();
 
