85617e87e2081571bd214e92be0cd07e6e1247f1
vinothkumar <evinoth24@gmail.com>
2018-08-23 19:54:22 +0530

    parallel execution scripts updated

--- Nbc_Automation/src/main/java/nbc_testcases/TC001_Logo_is_clickable.java
+++ Nbc_Automation/src/main/java/nbc_testcases/TC001_Logo_is_clickable.java
@@ -25,12 +25,13 @@ public class TC001_Logo_is_clickable extends ProjectMethods{
 	new NbcPage(driver, test)
 	.clicknbclogo();
 	
-	
-	try {
+	}
+}
+	/*try {
 		Thread.sleep(10000);
 	} catch (InterruptedException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	}
-}
+	}*/
+
