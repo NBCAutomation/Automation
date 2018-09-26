6957377a90b4d18ada12b4b878da9fa9d166e951
Deltrie Allen <deltrie.allen@nbcuni.com>
2018-09-26 09:55:56 -0400

    Correcting typo

--- spire/html/spire-radar-data-check.sh
+++ spire/html/spire-radar-data-check.sh
@@ -3,4 +3,4 @@ if [ "$(hostname)" == "ip-10-9-169-143" ]; then
   source /home/ec2-user/.bashrc;
   cd /home/ec2-user/automation-deploy/src/spire/html;
 fi
-npm run weatherDataCheck
+npm run weatherDataChecks
